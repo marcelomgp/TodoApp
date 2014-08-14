f578949c19b73be58d73c2ca53cd71ed5e27b178
menezes <marcelo.menezes@prodec.com.br>
2014-08-14 17:36:26 -0300

    Adicionadas funcionalidades para controle de tarefas.

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,4 +1,26 @@
 Todos.TodoController = Ember.ObjectController.extend({
+  actions: {
+    editTask: function() {
+      this.set('isEditing', true);
+    },
+    acceptChanges: function() {
+      this.set('isEditing', false);
+
+      if (Ember.isEmpty(this.get('model.name'))) {
+        this.send('removeTask');
+      } else {
+        this.get('model').save();
+      }
+    },
+    removeTask: function() {
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
+    }
+  },
+
+  isEditing: false,
+
   isCompleted: function(key, value){
     var model = this.get('model');
 
--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,5 +1,10 @@
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    },
     createTask: function() {
       var name = this.get('newName');
       if (!name) { return false; }
@@ -14,5 +19,31 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       task.save();
     }
-  }
+  },
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+
+  remaining: function() {
+    return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  incompleteTasks: function() {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'tarefa pendente' : 'tarefas pendentes';
+  }.property('remaining'),
+
+  allAreDone: function(key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.everyProperty('isCompleted', true);
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted')
 });
